9f76949c9efbe8386080fd0cd1a9752feee57037
Chamila de Alwis <cs@chamiladealwis.com>
2016-03-31 10:17:36 +0530

    Stop script for spawned containers

---
+++ common/scripts/docker-stop.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# ------------------------------------------------------------------------
+#
+# Copyright 2016 WSO2, Inc. (http://wso2.com)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License
+
+# ------------------------------------------------------------------------
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source "${DIR}/base.sh"
+
+while getopts :n: FLAG; do
+    case $FLAG in
+        n)
+            product_name=$OPTARG
+            ;;
+    esac
+done
+
+mapfile -t running_containers < <(docker ps | awk -F '[[:space:]][[:space:]]+' '{if (NR!=1) print $NF, "-", $1, "Started",$4}' | grep $product_name)
+
+if [ "${#running_containers[@]}" -eq 0 ]; then
+    echo "No running containers for $(echo $product_name | awk '{print toupper($0)}') was found."
+    exit 1
+fi
+
+for running_container in "${running_containers[@]}"
+do
+    container_id=$(echo $running_container | awk '{print $3}')
+    echo -n "${running_container}"
+    askBold " - Terminate? (y/n): "
+    read -r terminate_v
+    if [ "$terminate_v" == "y" ]; then
+        {
+            docker kill $container_id > /dev/null 2>&1 && echoSuccess "$(echo $running_container | awk '{print $1,"(",$3,")"}') was terminated."
+        } || {
+            echoError "Couldn't terminate container $(echo $running_container | awk '{print $1,"(",$3,")"}')."
+        }
+    fi
+done
