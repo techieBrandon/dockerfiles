031184b4602c698709d969624fd07f9ab55ab683
Chamila de Alwis <cs@chamiladealwis.com>
2016-04-01 07:32:40 +0530

    Minor stop script improvements

--- common/scripts/docker-stop.sh
+++ common/scripts/docker-stop.sh
@@ -31,11 +31,17 @@ read -r -a running_container_ids <<< $(docker ps | grep $product_name | awk '{pr
 if [ "${#running_container_ids[@]}" -eq 0 ]; then
     echo "No running containers for $(echo $product_name | awk '{print toupper($0)}') were found."
 else
-    echoBold "Found ${#running_container_ids[@]} containers matching $(echo $product_name | awk '{print toupper($0)}')"
+    if [ "${#running_container_ids[@]}" -gt 1 ]; then
+        echoBold "Found ${#running_container_ids[@]} containers matching $(echo $product_name | awk '{print toupper($0)}')"
+    fi
+
     for running_container_id in "${running_container_ids[@]}"
     do
-        running_container_info=$(docker ps -f "id=${running_container_id}" | awk -F '[[:space:]][[:space:]]+' '{if (NR!=1) print $NF,"-",$1,"Started", $4, "from image", $2}')
-        echo -n "${running_container_info}"
+        running_container_info=$(docker ps -f "id=${running_container_id}" | awk -F '[[:space:]][[:space:]]+' '{if (NR!=1) print $NF,$1,"Started", $4, "from image", $2}')
+        running_container_name=$(echo $running_container_info | awk '{print $1}')
+        running_container_meta=$(echo $running_container_info | awk '{$1=""; print $0}')
+        askBold "${running_container_name}"
+        echo -n " (${running_container_meta} )"
         askBold " - Terminate? (y/n): "
         read -r terminate_v
         if [ "$terminate_v" == "y" ]; then
@@ -48,14 +54,12 @@ else
     done
 fi
 
-echo
-askBold "Clean already exited containers? (y/n): "
-read -r clean_exited_v
-if [ "$clean_exited_v" == "y" ]; then
-    exited_container_ids=$(docker ps -q -f status=exited)
-    if [ -z "$exited_container_ids" ]; then
-        echo "No exited containers were found."
-    else
+exited_container_ids=$(docker ps -q -f status=exited)
+if [ ! -z "$exited_container_ids" ]; then
+    echo
+    askBold "Clean already exited containers? (y/n): "
+    read -r clean_exited_v
+    if [ "$clean_exited_v" == "y" ]; then
         {
             echo "Cleaning..." && docker rm $exited_container_ids > /dev/null 2>&1 && echoSuccess "Cleaned all exited containers."
         } || {
