389e8170dafb39fded9092950371612b4020a7e7
Akila Perera <ravihansa3000@gmail.com>
2016-07-08 12:59:19 +0530

    Fix wso2das init script extension for Mesos platform

--- wso2das/wso2das-default-init.sh
+++ wso2das/wso2das-default-init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # ------------------------------------------------------------------------
 #
-# Copyright 2005-2015 WSO2, Inc. (http://wso2.com)
+# Copyright 2016 WSO2, Inc. (http://wso2.com)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,10 +17,90 @@
 
 # ------------------------------------------------------------------------
 set -e
+EVENT_PROCESSOR_XML_FILE_PATH=${CARBON_HOME}/repository/conf/event-processor.xml
+SPARK_DEFAULTS_CONF_FILE_PATH=${CARBON_HOME}/repository/conf/analytics/spark/spark-defaults.conf
+SPARK_ENV_SCRIPT_FILE_PATH=${CARBON_HOME}/bin/load-spark-env-vars.sh
 
-local_ip=$(ip route get 1 | awk '{print $NF;exit}')
-server_path=/mnt/${local_ip}
-server_name=${WSO2_SERVER}-${WSO2_SERVER_VERSION}
+# Update Spark driver bind address to Docker runtime IP address
+sed -i "s/spark\.driver\.host.*/spark.driver.host ${LOCAL_DOCKER_IP}/g" ${SPARK_DEFAULTS_CONF_FILE_PATH} \
+&& echo "Replaced spark.driver.host with ${LOCAL_DOCKER_IP}"
 
-ln -s "${server_path}/${server_name}" '/mnt/carbon.das.symlink'
-echo "created symlink /mnt/carbon.das.symlink -> ${server_path}/${server_name}"
+# Due to lack of built-in overlay network in Mesos, we need to bind to host IP and dynamic proxy ports
+# This is only useful in standalone mode. Use DCOS Spark framework to deploy a Spark cluster in Mesos with wso2das running in Spark client mode
+if [[ $PLATFORM == "mesos" ]]; then
+  # replace eventSync/hostName
+  sed -i "/<eventSync>/,/<\/eventSync>/ s|<hostName>[0-9a-z.]\{1,\}</hostName>|<hostName>${HOST}</hostName>|g" ${EVENT_PROCESSOR_XML_FILE_PATH} \
+  && echo "Replaced eventSync/hostName with ${HOST}"
+
+  # replace eventSync/port
+  sed -i "/<eventSync>/,/<\/eventSync>/ s|<port>[0-9a-z.]\{1,\}</port>|<port>${PORT18}</port>|g" ${EVENT_PROCESSOR_XML_FILE_PATH} \
+  && echo "Replaced eventSync/port with ${PORT18}"
+
+  # replace management/hostName
+  sed -i "/<management>/,/<\/management>/ s|<hostName>[0-9a-z.]\{1,\}</hostName>|<hostName>${HOST}</hostName>|g" ${EVENT_PROCESSOR_XML_FILE_PATH} \
+  && echo "Replaced management/hostName with ${HOST}"
+
+  # replace management/port
+  sed -i "/<management>/,/<\/management>/ s|<port>[0-9a-z.]\{1,\}</port>|<port>${PORT19}</port>|g" ${EVENT_PROCESSOR_XML_FILE_PATH} \
+  && echo "Replaced management/port with ${PORT19}"
+
+  # replace presentation/hostName
+  sed -i "/<presentation>/,/<\/presentation>/ s|<hostName>[0-9a-z.]\{1,\}</hostName>|<hostName>${HOST}</hostName>|g" ${EVENT_PROCESSOR_XML_FILE_PATH} \
+  && echo "Replaced presentation/hostName with ${HOST}"
+
+  # replace presentation/port
+  sed -i "/<presentation>/,/<\/presentation>/ s|<port>[0-9a-z.]\{1,\}</port>|<port>${PORT16}</port>|g" ${EVENT_PROCESSOR_XML_FILE_PATH} \
+  && echo "Replaced presentation/port with ${PORT16}"
+
+  # Update Spark ports with dynamically generated Mesos port mappings
+  sed -i "s/spark\.ui\.port.*/spark.ui.port ${PORT5}/g" ${SPARK_DEFAULTS_CONF_FILE_PATH} \
+  && echo "Replaced spark.ui.port with ${PORT5}"
+
+  sed -i "s/spark\.history\.ui\.port.*/spark.history.ui.port ${PORT6}/g" ${SPARK_DEFAULTS_CONF_FILE_PATH} \
+  && echo "Replaced spark.history.ui.port with ${PORT6}"
+
+  sed -i "s/spark\.blockManager\.port.*/spark.blockManager.port ${PORT7}/g" ${SPARK_DEFAULTS_CONF_FILE_PATH} \
+  && echo "Replaced spark.blockManager.port with ${PORT7}"
+
+  sed -i "s/spark\.broadcast\.port.*/spark.broadcast.port ${PORT8}/g" ${SPARK_DEFAULTS_CONF_FILE_PATH} \
+  && echo "Replaced spark.broadcast.port with ${PORT8}"
+
+  sed -i "s/spark\.driver\.port.*/spark.driver.port ${PORT9}/g" ${SPARK_DEFAULTS_CONF_FILE_PATH} \
+  && echo "Replaced spark.driver.port with ${PORT9}"
+
+  sed -i "s/spark\.executor\.port.*/spark.executor.port ${PORT10}/g" ${SPARK_DEFAULTS_CONF_FILE_PATH} \
+  && echo "Replaced spark.executor.port with ${PORT10}"
+
+  sed -i "s/spark\.fileserver\.port.*/spark.fileserver.port ${PORT11}/g" ${SPARK_DEFAULTS_CONF_FILE_PATH} \
+  && echo "Replaced spark.fileserver.port with ${PORT11}"
+
+  sed -i "s/spark\.replClassServer\.port.*/spark.replClassServer.port ${PORT12}/g" ${SPARK_DEFAULTS_CONF_FILE_PATH} \
+  && echo "Replaced spark.replClassServer.port with ${PORT12}"
+
+  sed -i "s/spark\.master\.port.*/spark.master.port ${PORT13}/g" ${SPARK_DEFAULTS_CONF_FILE_PATH} \
+  && echo "Replaced spark.master.port with ${PORT13}"
+
+  sed -i "s/spark\.master\.rest\.port.*/spark.master.rest.port ${PORT14}/g" ${SPARK_DEFAULTS_CONF_FILE_PATH} \
+  && echo "Replaced spark.master.rest.port with ${PORT14}"
+
+  sed -i "s/spark\.master\.webui\.port.*/spark.master.webui.port ${PORT15}/g" ${SPARK_DEFAULTS_CONF_FILE_PATH} \
+  && echo "Replaced spark.master.webui.port with ${PORT15}"
+
+  sed -i "s/spark\.worker\.port.*/spark.worker.port ${PORT16}/g" ${SPARK_DEFAULTS_CONF_FILE_PATH} \
+  && echo "Replaced spark.worker.port with ${PORT16}"
+
+  sed -i "s/spark\.worker\.webui\.port.*/spark.worker.webui.port ${PORT17}/g" ${SPARK_DEFAULTS_CONF_FILE_PATH} \
+  && echo "Replaced spark.worker.webui.port with ${PORT17}"
+else
+  # replace eventSync/hostName
+  sed -i "/<eventSync>/,/<\/eventSync>/ s|<hostName>[0-9a-z.]\{1,\}</hostName>|<hostName>${LOCAL_DOCKER_IP}</hostName>|g" ${EVENT_PROCESSOR_XML_FILE_PATH} \
+  && echo "Replaced eventSync/hostName with ${LOCAL_DOCKER_IP}"
+
+  # replace management/hostName
+  sed -i "/<management>/,/<\/management>/ s|<hostName>[0-9a-z.]\{1,\}</hostName>|<hostName>${LOCAL_DOCKER_IP}</hostName>|g" ${EVENT_PROCESSOR_XML_FILE_PATH} \
+  && echo "Replaced management/hostName with ${LOCAL_DOCKER_IP}"
+
+  # replace presentation/hostName
+  sed -i "/<presentation>/,/<\/presentation>/ s|<hostName>[0-9a-z.]\{1,\}</hostName>|<hostName>${LOCAL_DOCKER_IP}</hostName>|g" ${EVENT_PROCESSOR_XML_FILE_PATH} \
+  && echo "Replaced presentation/hostName with ${LOCAL_DOCKER_IP}"
+fi
